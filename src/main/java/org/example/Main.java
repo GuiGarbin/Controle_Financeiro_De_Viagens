02a21a744e05690cd7afe86031bf62866db3f5e2
Davi <davi.zelski@gmail.com>
2026-04-08 15:44:19 -0300

    Added placeholder GUI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,37 @@
 package org.example;
-public class Main {
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label nameLabel = new Label("Digite seu nome");
+        TextField textField = new TextField(("Seu nome aqui"));
+        Button submitButton = new Button("Enviar");
+
+        VBox vbox = new VBox(8);
+        vbox.getChildren().addAll(nameLabel, textField, submitButton);
+
+        submitButton.setOnAction(e -> {
+            String name = textField.getText();
+            nameLabel.setText("Olá " + name);
+        });
+
+        Scene scene = new Scene(vbox, 300, 200);
+
+        stage.setTitle("Janela Protótipo");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
+        launch();
     }
 }
