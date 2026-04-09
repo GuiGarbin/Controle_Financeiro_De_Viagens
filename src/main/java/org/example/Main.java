dadede045f2c4b949d465a8cc7acf367a081f198
Davi <davi.zelski@gmail.com>
2026-04-09 14:16:23 -0300

    Added proper file structure for fxml and controller files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,20 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        Label nameLabel = new Label("Digite seu nome");
-        TextField textField = new TextField(("Seu nome aqui"));
-        Button submitButton = new Button("Enviar");
+    public void start(Stage stage) throws Exception {
 
-        VBox vbox = new VBox(8);
-        vbox.getChildren().addAll(nameLabel, textField, submitButton);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Prototype.fxml"));
+        VBox root = loader.load();
 
-        submitButton.setOnAction(e -> {
-            String name = textField.getText();
-            nameLabel.setText("Olá " + name);
-        });
-
-        Scene scene = new Scene(vbox, 300, 200);
+        Scene scene = new Scene(root, 300, 200);
 
         stage.setTitle("Janela Protótipo");
         stage.setScene(scene);
